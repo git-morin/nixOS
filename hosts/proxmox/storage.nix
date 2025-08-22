a9162d74596b1775d814fe1a310437e3dcafb678
git-morin <dev.gab.morin@gmail.com>
2025-08-21 21:45:06 -0400

    :wrench: fix eval...

--- hosts/proxmox/storage.nix
+++ hosts/proxmox/storage.nix
@@ -1,40 +1,40 @@
 {
-  virtualization.proxmox.storage = {
-      "directory" = {
-        type = "dir";
-        content = [
-          "images"
-          "iso"
-          "vztmpl"
-          "backup"
-          "snippets"
-        ];
-      };
-
-      "local" = {
-        type = "dir";
-        content = [
-          "iso"
-          "vztmpl"
-          "backup"
-          "snippets"
-        ];
-      };
-
-      "local-lvm" = {
-        type = "lvmthin";
-        content = [
-         "images"
-         "rootdir"
-        ];
-      };
-
-      "lvm-thin" = {
-        type = "lvmthin";
-        content = [
-        "images"
-        "rootdir"
-        ];
-      };
-    };
+#  virtualization.proxmox.storage = {
+#      "directory" = {
+#        type = "dir";
+#        content = [
+#          "images"
+#          "iso"
+#          "vztmpl"
+#          "backup"
+#          "snippets"
+#        ];
+#      };
+#
+#      "local" = {
+#        type = "dir";
+#        content = [
+#          "iso"
+#          "vztmpl"
+#          "backup"
+#          "snippets"
+#        ];
+#      };
+#
+#      "local-lvm" = {
+#        type = "lvmthin";
+#        content = [
+#         "images"
+#         "rootdir"
+#        ];
+#      };
+#
+#      "lvm-thin" = {
+#        type = "lvmthin";
+#        content = [
+#        "images"
+#        "rootdir"
+#        ];
+#      };
+#    };
 }
