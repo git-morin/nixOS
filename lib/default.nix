676428c1ca673409211923dae3b9ea25318a3167
git-morin <dev.gab.morin@gmail.com>
2025-09-06 03:10:06 -0400

    :wrench: fix eval

--- lib/default.nix
+++ lib/default.nix
@@ -1,57 +1,61 @@
-{ inputs ? null }:
-{
-  # Read all .nix files (except default.nix) and import them
-  importNixFiles = 
-  path: # path to scan
-    let
-      nixFiles = builtins.filter 
-        (n: n != "default.nix" && builtins.match ".*\\.nix$" n != null)
-        (builtins.attrNames (builtins.readDir path));
-    in
-      map (x: path + "/${x}") nixFiles;
-
-  # Parses the current directory for .nix files (except default.nix)
-  # and returns a list of actual package derivations
-  importHomeManagerPackages = 
-    path: # path for scanning
-    pkgs: # pkgs inputs
+let
+  mkLib = inputs: {
+    # Read all .nix files (except default.nix) and import them
+    importNixFiles =
+    path: # path to scan
       let
-        nixFiles = builtins.filter 
-          (n: builtins.match ".*\\.nix$" n != null && n != "default.nix")
+        nixFiles = builtins.filter
+          (n: n != "default.nix" && builtins.match ".*\\.nix$" n != null)
           (builtins.attrNames (builtins.readDir path));
-
-        packageNames = map (n: builtins.replaceStrings [".nix"] [""] n) nixFiles;
-
-        getPackage = pkg: 
-          if pkgs ? ${pkg} 
-          then pkgs.${pkg} 
-          else null;
       in
-        builtins.filter (p: p != null) (map getPackage packageNames);
+        map (x: path + "/${x}") nixFiles;
 
-  # Generate nixOS configuration from hosts
-  buildNixosConfiguration = hostname: hostConfig:
-    inputs.nixpkgs.lib.nixosSystem {
-      system = hostConfig.system or "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ../hosts/${hostname}
-        inputs.home-manager.nixosModules.home-manager
-        (import ../flakes/home-manager.nix {
-          inherit inputs;
-          system = hostConfig.system or "x86_64-linux";
-          userList = hostConfig.users or [];
-        })
-      ] ++ (hostConfig.additionalModules or []);
-    };
+    # Parses the current directory for .nix files (except default.nix)
+    # and returns a list of actual package derivations
+    importHomeManagerPackages =
+      path: # path for scanning
+      pkgs: # pkgs inputs
+        let
+          nixFiles = builtins.filter
+            (n: builtins.match ".*\\.nix$" n != null && n != "default.nix")
+            (builtins.attrNames (builtins.readDir path));
 
-  # Generate ISO configuration from hosts
-  buildIsoConfiguration = hostname: hostConfig:
-    inputs.nixpkgs.lib.nixosSystem {
-      system = hostConfig.system or "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ../iso/${hostname}.nix
-      ];
-    };
+          packageNames = map (n: builtins.replaceStrings [".nix"] [""] n) nixFiles;
+
+          getPackage = pkg:
+            if pkgs ? ${pkg}
+            then pkgs.${pkg}
+            else null;
+        in
+          builtins.filter (p: p != null) (map getPackage packageNames);
+
+    # Generate nixOS configuration from hosts
+    buildNixosConfiguration = hostname: hostConfig:
+      inputs.nixpkgs.lib.nixosSystem {
+        system = hostConfig.system or "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ../hosts/${hostname}
+          inputs.home-manager.nixosModules.home-manager
+          (import ../flakes/home-manager.nix {
+            inherit inputs;
+            system = hostConfig.system or "x86_64-linux";
+            userList = hostConfig.users or [];
+          })
+        ] ++ (hostConfig.additionalModules or []);
+      };
+
+    # Generate ISO configuration from hosts
+    buildIsoConfiguration = hostname: hostConfig:
+      inputs.nixpkgs.lib.nixosSystem {
+        system = hostConfig.system or "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ../iso/${hostname}.nix
+        ];
+      };
+  };
+in
+(mkLib null) // {
+  withInputs = mkLib;
 }
