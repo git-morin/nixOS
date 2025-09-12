49cdb2cfdfa778e28cd22a576ad31936fab88cd7
git-morin <dev.gab.morin@gmail.com>
2025-09-12 09:19:15 -0400

    :wrench: add nix-ld for wsl (vs-code wsl extension)

---
+++ hosts/wsl/programs.nix
@@ -0,0 +1,14 @@
+{
+  programs.nix-ld.enable = true;
+
+  programs.nix-ld.libraries = with pkgs; [
+      stdenv.cc.cc.lib
+      zlib
+      fuse3
+      icu
+      nss
+      openssl
+      curl
+      expat
+    ];
+}
