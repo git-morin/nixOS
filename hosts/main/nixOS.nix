45d578a8211bf59862bb770449882ba29b21a206
git-morin <dev.gab.morin@gmail.com>
2025-09-18 23:27:18 -0400

    :broom: cleanup thrice

--- hosts/main/nixOS.nix
+++ hosts/main/nixOS.nix
@@ -3,9 +3,5 @@
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "nvidia-x11"  # nvidia is not open-source
     "nvidia-settings"  # nvidia is not open-source
-    "steam"  # steam is not open-source
-    "steam-original"  # steam is not open-source
-    "steam-unwrapped"  # steam is not open-source
-    "steam-run"  # steam is not open-source
   ];
 }
