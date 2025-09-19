b8bcabf8fa65d98b00045d4f7f56ecf97523644c
git-morin <dev.gab.morin@gmail.com>
2025-09-18 23:27:18 -0400

    :broom: cleanup thrice

--- hosts/main/programs.nix
+++ hosts/main/programs.nix
@@ -1,11 +1,5 @@
 {
   programs = {
     firefox.enable = true;
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-    };
   };
 }
